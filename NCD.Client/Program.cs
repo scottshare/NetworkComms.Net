bfd81d7044f1f21222f2ba690d309eee767bb0a2
王涛 <wenney_esoft@live.cn>
2018-02-01 19:51:20 +0800

    Add TCP Client Demo And TCP Server

---
+++ NCD.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Connections.TCP;
+using NetworkCommsDotNet.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCD.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkComms.ConnectionEstablishTimeoutMS = 10000000;
+
+            //Send a message which will attempt to connect to local
+            TCPConnection.GetConnection(new ConnectionInfo(IPTools.ParseEndPointFromString(System.Configuration.ConfigurationManager.AppSettings["socketServer"]))).SendObject("Message", "hello!");
+
+            while (true)
+            {
+                string content = Console.ReadLine();
+                if (content.Equals("exit"))
+                    break;
+                TCPConnection.GetConnection(new ConnectionInfo(IPTools.ParseEndPointFromString(System.Configuration.ConfigurationManager.AppSettings["socketServer"]))).SendObject("Message", content);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ NCD.Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCD.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkComms.ConnectionEstablishTimeoutMS = 10000000;
+
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", (packetHeader, connection, incomingString) =>
+            {
+                Console.WriteLine("\n  ... Incoming message from " + connection.ToString() + " saying '" + incomingString + "'.");
+            });
+
+            //Start listening for incoming 'TCP' connections.
+            Connection.StartListening(ConnectionType.TCP, IPTools.ParseEndPointFromString(System.Configuration.ConfigurationManager.AppSettings["listenAddress"]));
+            Console.ReadKey(true);
+            NetworkComms.Shutdown();
+        }
+    }
+}
